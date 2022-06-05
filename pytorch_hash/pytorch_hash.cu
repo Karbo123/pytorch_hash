5f75096a3563b1c901f33fe31ac66cd8e5bb430c
lei.jiabao <karbolei@outlook.com>
2022-06-05 15:34:44 +0800

    use stream

--- pytorch_hash/pytorch_hash.cu
+++ pytorch_hash/pytorch_hash.cu
@@ -4,6 +4,7 @@
       output data has shape == (num, )
 */
 #include <torch/extension.h>
+#include <ATen/cuda/CUDAContext.h>
 #include <pybind11/pybind11.h>
 #include <thrust/for_each.h>
 #include <thrust/execution_policy.h>
@@ -21,7 +22,7 @@ struct hash_functor {
         uint64_t number = dim * sizeof(scalar_t);
         uint64_t result = 0XCBF29CE484222325;
         while (number--) result = (result ^ (*ptr_in_i++)) * 0x00000100000001B3;
-        ptr_out[index_row] = *reinterpret_cast<int64_t*>(&result);
+        ptr_out[index_row] = reinterpret_cast<int64_t&>(result);
     }
 };
 
@@ -30,15 +31,24 @@ torch::Tensor hash(torch::Tensor data) {
     auto num = data.size(0);
     auto dim = data.size(1);
     auto is_cuda = data.is_cuda();
+
     auto options = torch::TensorOptions().dtype(torch::kInt64);
     torch::Tensor out = data.new_empty({num}, options);
+
     AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Half, data.type(), "hash", [&] {
         auto functor = hash_functor<scalar_t>(data.data_ptr<scalar_t>(), 
                                               out .data_ptr<int64_t>(),
                                               num, dim);
         auto iter_rows = thrust::counting_iterator<uint64_t>(0);
-        if (is_cuda) thrust::for_each_n(thrust::device, iter_rows, num, functor);
-        else         thrust::for_each_n(thrust::host,   iter_rows, num, functor);
+        if (is_cuda) {
+            auto stream = at::cuda::getCurrentCUDAStream();
+            auto device = thrust::cuda::par.on(stream);
+            thrust::for_each_n(device, iter_rows, num, functor);
+            cudaStreamSynchronize(stream);
+        }
+        else {
+            thrust::for_each_n(thrust::host, iter_rows, num, functor);
+        }
     });
     
     return out;
